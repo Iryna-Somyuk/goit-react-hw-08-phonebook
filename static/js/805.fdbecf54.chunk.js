dacc29dcb31192d0431d04bc640714ac9cc082d5
Iryna-Somyuk <107417578+Iryna-Somyuk@users.noreply.github.com>
2023-03-19 17:39:54 +0000

    Deploying to gh-pages from @ Iryna-Somyuk/goit-react-hw-08-phonebook@1e6dc2c78f392c1b51c9b7936b4b19bffb6597e3 🚀

--- static/js/805.fdbecf54.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{9805:function(e,t,r){r.r(t);var a=r(1087),c=r(184);t.default=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)(a.OL,{to:"/",children:"Go back"}),(0,c.jsx)("img",{src:"https://d2x3xhvgiqkx42.cloudfront.net/12345678-1234-1234-1234-1234567890ab/2016/12/22/ae36c0d0-52ea-4a4a-a691-bec9b2458f5b.png",alt:"error 404",style:{borderRadius:"5px"}})]})}}}]);
-//# sourceMappingURL=805.fdbecf54.chunk.js.map
